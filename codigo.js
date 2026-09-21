da9414ac7f08cf4e7aaa84490c094d1fd58f51be
MarcoPY09 <marcoperezyoc@gmail.com>
2026-09-21 13:24:19 -0600

    adicion de imagenes

--- codigo.js
+++ codigo.js
@@ -12,3 +12,34 @@ function Limpiar(){
 function Operar(){
     pantalla.value = eval(pantalla.value)
 }
+
+const carruselElemento = document.querySelector(".carrusel-items");
+
+let posicionMaximaDesplazamiento = carruselElemento.scrollWidth - carruselElemento.clientWidth;
+let intervaloDesplazamiento = null;
+let pasoDesplazamiento = 1;
+
+const iniciarDesplazamiento = () => {
+  intervaloDesplazamiento = setInterval(function () {
+    carruselElemento.scrollLeft = carruselElemento.scrollLeft + pasoDesplazamiento;
+    if (carruselElemento.scrollLeft === posicionMaximaDesplazamiento) {
+      pasoDesplazamiento = pasoDesplazamiento * -1;
+    } else if (carruselElemento.scrollLeft === 0) {
+      pasoDesplazamiento = pasoDesplazamiento * -1;
+    }
+  }, 10);
+};
+
+const detenerDesplazamiento = () => {
+  clearInterval(intervaloDesplazamiento);
+};
+
+carruselElemento.addEventListener("mouseover", () => {
+  detenerDesplazamiento();
+});
+
+carruselElemento.addEventListener("mouseout", () => {
+  iniciarDesplazamiento();
+});
+
+iniciarDesplazamiento();
