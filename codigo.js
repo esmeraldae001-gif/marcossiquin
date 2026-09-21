04e52408782792a41ba9f8dec2883163a910a80a
MarcoPY09 <marcoperezyoc@gmail.com>
2026-09-21 11:41:17 -0600

    jorge

---
+++ codigo.js
@@ -0,0 +1,14 @@
+let pantalla = document.getElementById("pantalla")
+
+function Agregar(dato) {
+    pantalla.value += dato
+    
+}
+
+function Limpiar(){
+    pantalla.value = " "
+}
+
+function Operar(){
+    pantalla.value = eval(pantalla.value)
+}
